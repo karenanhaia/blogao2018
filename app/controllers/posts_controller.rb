11f57c88a8c836937fe7969700e9733f1c734e2f
Karen Anhaia <karenanhaia@hotmail.com>
2018-04-26 17:33:04 -0300

    Atualização Aula 26/04 1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -74,6 +74,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:text, :subject, :title, :author_id)
+      params.require(:post).permit(:text, :subject, :title, :author_id, :category_id)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :author
+  belongs_to :category
 end
